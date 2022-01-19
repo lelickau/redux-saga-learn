6b4aebe83735da92dac60f1a67f4b681368d463b
elena_kau <lelickau@yandex.ru>
2022-01-19 13:16:58 +0200

    асинхронные действия / запросы при входе в приложение

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,47 +1,29 @@
-import {call, fork, spawn, all} from 'redux-saga/effects'
+import {call, spawn, all, fork, delay} from 'redux-saga/effects'
 
-export function* saga1 () {
-    console.log('saga1');
+// асинхронные действия / запросы при входе в приложение
+function* auth() {
+    // delay() - ожидать(n секунд) (синхронный 'setTimeout')
+    yield delay(2000)
+    console.log('auth ok');
+    return true
 }
-export function* saga2 () {
-    console.log('saga2');
+
+function* loadUsers() {
+    const req = yield call(fetch, 'https://jsonplaceholder.typicode.com/users')
+    const data = yield call([req, req.json])
+    console.log('data', data);
 }
-export function* saga3 () {
-    console.log('saga3');
+
+export function* loadBasicData () {
+    yield all([
+        fork(auth),
+        fork(loadUsers)
+    ])
 }
+
 
 export default function* rootSaga() {
-    // способы вызова
-    // 1 способ: fork создает не блокирующий вызов - code будет выполнен сразу после запуска yield в случае ошибки любой из саг - последующие процессы будут отменены, а rootSaga не будет более выполняться, при дальнейшем вызове ничего происходить не будет
-    // yield [
-    //     fork(saga1),
-    //     fork(saga2),
-    //     fork(saga3),
-    // ]
-    // code
-
-    // 2 способ: саги запустятся параллельно, rootSaga будет заблокирована пока не исполнятся все саги, в случае ошибки любой из саг - последующие процессы будут отменены, а rootSaga не будет более выполняться, при дальнейшем вызове ничего происходить не будет
-    // yield [
-    //     saga1(),
-    //     saga2(),
-    //     saga3(),
-    // ]
-    // code
-
-    // 3 способ: отработает так же как 1 способ
-    // yield fork(saga1);
-    // yield fork(saga2);
-    // yield fork(saga3);
-    // code
-
-    // 4 способ: spawn создает распределенную задачу в корне процессов - саги будут разделены друг от друга и от родительской задачи, в случае ошибки любой из саг - процессы не остановятся
-    // yield spawn(saga1); // auth
-    // yield spawn(saga2); // users
-    // yield spawn(saga3); // payments
-    // code
-
-    // 5 способ
-    const sagas = [saga1, saga2, saga3]
+    const sagas = [loadBasicData]
 
     const retrySagas = sagas.map(saga => {
         return spawn(function* () {
